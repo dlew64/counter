211b46a24a871a42e1f9e59820541574411ec702
dlew64 <daniel.lewis.fl@gmail.com>
2022-12-04 23:45:31 -0500

    Revert "Add tests for the Counter component"
    This reverts commit 04a4980c00178a7677be70c9e597928cc6dcecef.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,32 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import Counter from '../components/Counter';
-import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom';
+// import necessary react testing library helpers here
+// import the Counter component here
 
 beforeEach(() => {
   // Render the Counter component here
-  render(<Counter/>);
 })
 
 test('renders counter message', () => {
-  const welcomeMessage = screen.getByText(/Counter/i);
-  expect(welcomeMessage).toBeInTheDocument();
+  // Complete the unit test below based on the objective in the line above
 });
 
 test('should render initial count with value of 0', () => {
-  const intial_val = screen.getByTestId("count");
-  expect(intial_val).toHaveTextContent(0);
-  
+  // Complete the unit test below based on the objective in the line above
 });
 
 test('clicking + increments the count', () => {
-  const intial_val = screen.getByTestId("count");
-  userEvent.click(screen.getByText('+'));
-  expect(intial_val).toHaveTextContent(1);
+  // Complete the unit test below based on the objective in the line above
 });
 
 test('clicking - decrements the count', () => {
-  const intial_val = screen.getByTestId("count");
-  userEvent.click(screen.getByText('-'));
-  expect(intial_val).toHaveTextContent(-1);
+  // Complete the unit test below based on the objective in the line above
 });
